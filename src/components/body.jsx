0666b9c5d483d0ce4cc552d67e0008e146ca9b87
denkiwakame <denkivvakame@gmail.com>
2025-02-28 17:59:07 +0900

    add code highlighter

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { render } from 'react-dom';
 import { marked } from 'marked';
 import markedKatex from 'marked-katex-extension';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/base16/gruvbox-dark-hard.css';
+//import 'highlight.js/styles/base16/github.css';
+//import 'highlight.js/styles/tokyo-night-dark.css';
+// import 'highlight.js/styles/pojoaque.css';
 
 const renderer = new marked.Renderer();
 renderer.table = (header, body) => {
   return `<div class="uk-overflow-auto uk-width-1-1"><table class="uk-table uk-table-small uk-text-small uk-table-divider"> ${header} ${body} </table></div>`;
 };
+renderer.code = (code, language) => {
+  return `<pre class="hljs"><code class="hljs language-${language}">${code}</code></pre>`;
+};
 
 marked.use(markedKatex({ throwOnError: false }));
+marked.use(
+  markedHighlight({
+    emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
 marked.use({ renderer: renderer });
 
 class Content extends React.Component {
